d34f8fda4da2dc355bbcbbe1b115c183b0ab713d
lumrachele <rlum894@gmail.com>
2019-05-12 23:48:15 -0400

    style header

--- screens/Main.js
+++ screens/Main.js
@@ -21,9 +21,25 @@ export default class Main extends React.Component {
   render() {
     return (
           <View style={styles.container}>
+
             <View style={styles.header}>
-            <Text style={styles.headerText}>TO DO LIST</Text>
+              <Text style={styles.headerText}>
+              TO DO LIST
+              </Text>
             </View>
+
+            <ScrollView style={styles.scrollContainer}>
+            </ScrollView>
+
+            <View style={styles.footer}>
+              <TextInput style={styles.textInput}
+              placeholder="to do"
+              placeholderTextColor='white'
+              >
+
+              </TextInput>
+            </View>
+
           </View>
     );
   }
@@ -35,9 +51,33 @@ const styles = StyleSheet.create({
     // backgroundColor: '#fff',
   },
   header: {
-
+    backgroundColor: '#e91e63',
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomWidth: 10,
+    borderBottomColor: '#ddd'
   },
   headerText: {
+    color: 'white',
+    fontSize: 30,
+    padding: 26
+  },
+  scrollContainer: {
+    flex: 1,
+    marginBottom: 100
+  },
+  footer:{
+    position: 'absolute',
+    zIndex: 10
+  },
+  textInput:{
+    alignSelf: 'stretch',
+    color: '#fff',
+    padding: 20,
+    backgroundColor: '#252525',
+    borderTopWidth: 2,
+    borderTopColor: '#ededed',
+
 
   }
 });
