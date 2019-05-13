8f6c72e4a4f95f49c807ab542e814a4935f13fef
lumrachele <rlum894@gmail.com>
2019-05-13 14:01:39 -0400

    add button and styling

--- screens/Main.js
+++ screens/Main.js
@@ -40,7 +40,15 @@ export default class Main extends React.Component {
               </TextInput>
             </View>
 
+            <TouchableOpacity style={styles.addButton}>
+              <Text style={styles.addButtonText}>
+              +
+              </Text>
+            </TouchableOpacity>
+
+
           </View>
+
     );
   }
 }
@@ -68,6 +76,9 @@ const styles = StyleSheet.create({
   },
   footer:{
     position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
     zIndex: 10
   },
   textInput:{
@@ -75,9 +86,24 @@ const styles = StyleSheet.create({
     color: '#fff',
     padding: 20,
     backgroundColor: '#252525',
-    borderTopWidth: 2,
+    borderTopWidth: 5,
     borderTopColor: '#ededed',
-
-
+  },
+  addButton:{
+    position: 'absolute',
+    zIndex: 11,
+    right: 20,
+    bottom: 90,
+    backgroundColor: '#e91e63',
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 8
+  },
+  addButtonText: {
+    color: '#fff',
+    fontSize: 24,
   }
 });
